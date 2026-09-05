a4ef035b306807dd0de34b8b29ecceb9a1db2e1a
codercms <coder1994@gmail.com>
2026-09-05 17:14:04 +0500

    chore: linter fixes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -100,7 +100,7 @@ func run() int {
 			ctxDoneCh = nil
 			timeoutCh = time.After(shutdownTimeout)
 
-		case _ = <-timeoutCh:
+		case <-timeoutCh:
 			logger.Error("Failed to graceful shutdown", slog.String("duration", shutdownTimeout.String()))
 
 			return 1
